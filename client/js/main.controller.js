c7ba0933cf8440c75275bfe086e617c45a545911
dsreenevasan <dsreenevasan@gmail.com>
2016-12-19 19:04:34 +0530

    added alternate bootstrap

--- client/js/main.controller.js
+++ client/js/main.controller.js
@@ -5,14 +5,24 @@
     .module('abacus')
     .controller('MainController', MainController);
 
-  MainController.$inject = ['$state', '$compile', '$scope'];
+  MainController.$inject = ['$state', '$compile', '$scope', '$window'];
 
-  function MainController($state, $compile, $scope){
+  function MainController($state, $compile, $scope, $window){
 
     var ctrl = this;
     console.log("Success!");
     ctrl.showNav = false;
     ctrl.maxWidth = 0;
+    detect();
+    
+    function detect () {
+      angular.element(window).bind("scroll", function(){
+        ctrl.page = window.pageYOffset;
+        console.log(ctrl.page);
+      });
+    }
+    ctrl.page = window.pageYOffset;
+    console.log(ctrl.page);
 
     ctrl.displayNav = function() {
       ctrl.maxWidth = ctrl.maxWidth === 0 ? '50px' : 0;
